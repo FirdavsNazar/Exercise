7313341a0deabbe68a64e571fdb5f42fc4c104fe
Fredrick <fredrickuzb55@gmail.com>
2024-03-21 11:02:04 +0900

    tasks for js

---
+++ one.js
@@ -0,0 +1,94 @@
+// let user = {
+//     name: "Abu",
+//     age: 23,
+//     new: Symbol("Hello World")
+// }
+
+// let abc = Object.entries(user)
+
+// console.log(abc);
+
+
+////////////////////////////////////////////////////////////////////////
+
+// let arr1 = [1,-2,3,4];
+// let arr2 = [8,3,-8,1];
+
+// console.log(typeof new Map());
+
+///////////////////////////////////////////////////////////////////////
+
+// let map = new Map();
+// map.set(1, 'abs');
+// console.log(map.get(1)); 
+
+//////////////////////////////////////////////////////////////////////
+
+// let set = new Set()
+// set.add("abc")
+
+// let a = [...set][0]
+
+// let b = "abc"
+
+// console.log(a == b);
+
+//////////////////////////////////////////////////////////////////////////
+
+// function f(a , s , ...b){
+//     console.log(typeof arguments);
+// }
+
+// f(1,2,3,4,5)
+
+
+
+// function abc(js = function(){}){
+//     return [1,2,3,4,5]
+// }
+
+// console.log(abc.length);
+
+
+// const firdavs = function abs (){
+//     console.log('hello', firdavs.name);
+// }
+
+// firdavs()
+
+
+// setTimeout(function(){
+//     console.log('abs 1')
+// })
+// console.log("abs 2")
+
+// let arr = [4,2,3]
+// arr.with = [1, 42]
+
+// console.log(arr);
+
+
+
+// let arr  = [4, 2, 3];
+// let newArr = arr.toReversed()
+// console.log( newArr );
+
+// try {
+//     let helloworld = "Hello World"
+//     console.log(helloworld)
+// } catch(err){
+//     console.log("Error");
+// }
+
+
+// try {
+//     console.log("helloworld")
+//     helloworld
+// } catch(err){
+//     console.log("Error");
+// }
+
+console.log(Number.MAX_VALUE - Number.MIN_VALUE  > 0);
+    
+
+
